ede174e36aa2a15a16907a6c6786bf696e1e8b8b
James Robert Huggins Ngo <jamesrobert.ngo@gmail.com>
2018-08-05 13:12:31 -0400

    Add Event Properties Script

---
+++ src/scripts/essentials/eventproperties.js
@@ -0,0 +1,78 @@
+/* exported eventProperties */
+const eventProperties = {
+	eventHandlers: {
+		writable: true
+	},
+
+	off: {
+		value: function(eventName, eventHandler) {
+			if (eventName) {
+				if (typeof eventName === 'string') {
+					eventName.split(/\s+/).forEach((eventName) => {
+						if (this.eventHandlers && this.eventHandlers[eventName]) {
+							if (eventHandler) {
+								const index = this.eventHandlers[eventName].indexOf(eventHandler);
+								if (index !== -1) {
+									this.eventHandlers[eventName].splice(index, 1);
+								}
+								if (this.eventHandlers[eventName].length === 0) {
+									delete this.eventHandlers[eventName];
+								}
+							} else {
+								delete this.eventHandlers[eventName];
+							}
+						}
+					});
+				} else if (Array.isArray(eventName)) {
+					eventName.forEach((args) => {
+						this.off(...args);
+					});
+				}
+			}
+		}
+	},
+
+	on: {
+		value: function(eventName, eventHandler) {
+			if (eventName) {
+				if (typeof eventName === 'string') {
+					eventName.split(/\s+/).forEach((eventName) => {
+						if (eventHandler) {
+							if (!this.eventHandlers) {
+								this.eventHandlers = {};
+							}
+							if (!this.eventHandlers[eventName]) {
+								this.eventHandlers[eventName] = [];
+							}
+							this.eventHandlers[eventName].push(eventHandler);
+						}
+					});
+				} else if (Array.isArray(eventName)) {
+					eventName.forEach((args) => {
+						this.on(...args);
+					});
+				}
+			}
+		}
+	},
+
+	trigger: {
+		value: function(eventName, ...args) {
+			if (eventName) {
+				if (typeof eventName === 'string') {
+					eventName.split(/\s+/).forEach((eventName) => {
+						if (this.eventHandlers && this.eventHandlers[eventName]) {
+							this.eventHandlers[eventName].forEach((eventHandler) => {
+								eventHandler.call(this, ...args);
+							});
+						}
+					});
+				} else if (Array.isArray(eventName)) {
+					eventName.forEach((args) => {
+						this.trigger(...args);
+					});
+				}
+			}
+		}
+	}
+};
