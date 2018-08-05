7bfa3c0aa4f934c9c5ec54caef031bb368d6978f
James Robert Huggins Ngo <jamesrobert.ngo@gmail.com>
2018-08-05 14:41:48 -0400

    Add Check for Event Properties

--- src/scripts/essentials/mutationobserver.js
+++ src/scripts/essentials/mutationobserver.js
@@ -2,35 +2,37 @@
 
 /* exported mutationObserver */
 const mutationObserver = new MutationObserver((mutationsList) => {
-	if (!window.hasOwnProperty('trigger')) {
+	if (!window.hasOwnProperty('trigger') && eventProperties) {
 		Object.defineProperties(window, eventProperties);
 	}
 
-	window.trigger('domchanges', mutationsList);
+	if (window.hasOwnProperty('trigger')) {
+		window.trigger('domchanges', mutationsList);
 
-	mutationsList.forEach((mutationRecord) => {
-		window.trigger(`domchange domchange:${mutationRecord.type}`, mutationRecord, mutationsList);
+		mutationsList.forEach((mutationRecord) => {
+			window.trigger(`domchange domchange:${mutationRecord.type}`, mutationRecord, mutationsList);
 
-		if (mutationRecord.type === 'childList') {
-			mutationRecord.addedNodes.forEach(((node) => {
-				window.trigger(`domchange:childList:addNode`, node, mutationRecord, mutationsList);
+			if (mutationRecord.type === 'childList') {
+				mutationRecord.addedNodes.forEach(((node) => {
+					window.trigger(`domchange:childList:addNode`, node, mutationRecord, mutationsList);
 
-				if (node.nodeType === Node.ELEMENT_NODE) {
-					window.trigger(`domchange:childList:addElement domchange:childList:addElement:${node.tagName}`, node, mutationRecord, mutationsList);
-				}
-			}));
+					if (node.nodeType === Node.ELEMENT_NODE) {
+						window.trigger(`domchange:childList:addElement domchange:childList:addElement:${node.tagName}`, node, mutationRecord, mutationsList);
+					}
+				}));
 
-			mutationRecord.removedNodes.forEach(((node) => {
-				window.trigger(`domchange:childList:removedNode`, node, mutationRecord, mutationsList);
+				mutationRecord.removedNodes.forEach(((node) => {
+					window.trigger(`domchange:childList:removedNode`, node, mutationRecord, mutationsList);
 
-				if (node.nodeType === Node.ELEMENT_NODE) {
-					window.trigger(`domchange:childList:removedElement domchange:childList:removedElement:${node.tagName}`, node, mutationRecord, mutationsList);
-				}
-			}));
-		} else if (mutationRecord.type === 'attributes') {
-			// TO DO
-		}
-	});
+					if (node.nodeType === Node.ELEMENT_NODE) {
+						window.trigger(`domchange:childList:removedElement domchange:childList:removedElement:${node.tagName}`, node, mutationRecord, mutationsList);
+					}
+				}));
+			} else if (mutationRecord.type === 'attributes') {
+				// TO DO
+			}
+		});
+	}
 });
 
 mutationObserver.observe(document.body, {
