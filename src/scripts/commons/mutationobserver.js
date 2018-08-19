0782fbcf3241f4f63c95d3ac214c11d4a24f4df2
James Robert Huggins Ngo <jamesrobert.ngo@gmail.com>
2018-08-18 22:51:03 -0400

    Spread Out Event Names Simplify Legibility and Maintenance

--- src/scripts/commons/mutationobserver.js
+++ src/scripts/commons/mutationobserver.js
@@ -12,36 +12,74 @@ const mutationObserver = new MutationObserver((mutationsList) => {
 			if (mutationRecord.type === 'childList') {
 				mutationRecord.addedNodes.forEach(((node) => {
 					window.trigger([
-						[`nodeadded nodeadded:${node.nodeName.toUpperCase()}`, node],
-						[`childnodeadded childnodeadded:${targetNode.nodeName.toUpperCase()} childnodeadded:${targetNode.nodeName.toUpperCase()}:${node.nodeName.toUpperCase()}`, targetNode, node]
+						[[
+							'nodeadded',
+							`nodeadded:${node.nodeName.toUpperCase()}`
+						].join(' '), node],
+						[[
+							'childnodeadded',
+							`childnodeadded:${targetNode.nodeName.toUpperCase()}`,
+							`childnodeadded:${targetNode.nodeName.toUpperCase()}:${node.nodeName.toUpperCase()}`
+						].join(' '), targetNode, node]
 					]);
 
 					if (node.nodeType === Node.ELEMENT_NODE) {
 						window.trigger([
-							[`elementadded elementadded:${node.nodeName.toUpperCase()}`, node],
-							[`childelementadded childelementadded:${targetNode.nodeName.toUpperCase()} childelementadded:${targetNode.nodeName.toUpperCase()}:${node.nodeName.toUpperCase()}`, targetNode, node]
+							[[
+								'elementadded',
+								`elementadded:${node.nodeName.toUpperCase()}`
+							].join(' '), node],
+							[[
+								'childelementadded',
+								`childelementadded:${targetNode.nodeName.toUpperCase()}`,
+								`childelementadded:${targetNode.nodeName.toUpperCase()}:${node.nodeName.toUpperCase()}`
+							].join(' '), targetNode, node]
 						]);
 					}
 				}));
 
 				mutationRecord.removedNodes.forEach(((node) => {
 					window.trigger([
-						[`noderemoved noderemoved:${node.nodeName.toUpperCase()}`, node],
-						[`childnoderemoved childnoderemoved:${targetNode.nodeName.toUpperCase()} childnoderemoved:${targetNode.nodeName.toUpperCase()}:${node.nodeName.toUpperCase()}`, targetNode, node]
+						[[
+							'noderemoved',
+							`noderemoved:${node.nodeName.toUpperCase()}`
+						].join(' '), node],
+						[[
+							'childnoderemoved',
+							`childnoderemoved:${targetNode.nodeName.toUpperCase()}`,
+							`childnoderemoved:${targetNode.nodeName.toUpperCase()}:${node.nodeName.toUpperCase()}`
+						].join(' '), targetNode, node]
 					]);
 
 					if (node.nodeType === Node.ELEMENT_NODE) {
 						window.trigger([
-							[`elementremoved elementremoved:${node.nodeName.toUpperCase()}`, node],
-							[`childelementremoved childelementremoved:${targetNode.nodeName.toUpperCase()} childelementremoved:${targetNode.nodeName.toUpperCase()}:${node.nodeName.toUpperCase()}`, targetNode, node]
+							[[
+								'elementremoved',
+								`elementremoved:${node.nodeName.toUpperCase()}`
+							].join(' '), node],
+							[[
+								'childelementremoved',
+								`childelementremoved:${targetNode.nodeName.toUpperCase()}`,
+								`childelementremoved:${targetNode.nodeName.toUpperCase()}:${node.nodeName.toUpperCase()}`
+							].join(' '), targetNode, node]
 						]);
 					}
 				}));
 			} else if (mutationRecord.type === 'attributes') {
-				window.trigger(`nodeattributechange nodeattributechange:* nodeattributechange:*:${mutationRecord.attributeName} nodeattributechange:${targetNode.nodeName.toUpperCase()} nodeattributechange:${targetNode.nodeName.toUpperCase()}:${mutationRecord.attributeName}`, targetNode, mutationRecord.attributeName);
+				window.trigger([
+					'nodeattributechange',
+					`nodeattributechange:*:${mutationRecord.attributeName}`,
+					`nodeattributechange:${targetNode.nodeName.toUpperCase()}`,
+					`nodeattributechange:${targetNode.nodeName.toUpperCase()}:${mutationRecord.attributeName}`
+				].join(' '), targetNode, mutationRecord.attributeName);
 
 				if (targetNode.nodeType === Node.ELEMENT_NODE) {
-					window.trigger(`elementattributechange elementattributechange:* elementattributechange:*:${mutationRecord.attributeName} elementattributechange:${targetNode.nodeName.toUpperCase()} elementattributechange:${targetNode.nodeName.toUpperCase()}:${mutationRecord.attributeName}`, targetNode, mutationRecord.attributeName);
+					window.trigger([
+						'elementattributechange',
+						`elementattributechange:*:${mutationRecord.attributeName}`,
+						`elementattributechange:${targetNode.nodeName.toUpperCase()}`,
+						`elementattributechange:${targetNode.nodeName.toUpperCase()}:${mutationRecord.attributeName}`
+					].join(' '), targetNode, mutationRecord.attributeName);
 				}
 			}
 		});
@@ -57,7 +95,10 @@ mutationObserver.observe(document.body, {
 document.createElement = ((createElement) => {
 	return function(tagName, options) {
 		const element = createElement.call(this, tagName, options);
-		window.trigger(`elementcreated elementcreated:${element.nodeName.toUpperCase()}`, element);
+		window.trigger([
+			'elementcreated',
+			`elementcreated:${element.nodeName.toUpperCase()}`
+		].join(' '), element);
 		return element;
 	}
 })(document.createElement);
