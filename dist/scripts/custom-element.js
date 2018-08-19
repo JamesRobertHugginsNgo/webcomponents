74640e0d4f8ad63f1ac40b83f8f8be65b3e0aed1
James Robert Huggins Ngo <jamesrobert.ngo@gmail.com>
2018-08-18 21:49:02 -0400

    Separate Custom Element Example

--- dist/scripts/custom-element.js
+++ dist/scripts/custom-element.js
@@ -1,21 +1,17 @@
-(() => {
-	// GLOBAL STYLES
-	const style = document.createElement('style');
-	style.appendChild(document.createTextNode('')); // WebKit hack :(
-	document.head.appendChild(style);
-	style.sheet.insertRule(`
-		CUSTOM-ELEMENT {
-			color: red;
-			display: block;
-		}
-	`);
+/* global eventProperties */
 
-	// SHARED RESOURCES
+(() => {
+	const component = 'CUSTOM-ELEMENT';
+
+	/////////////////////////////////////////////////////////////////////////////
+	// SHARED CODE
+	/////////////////////////////////////////////////////////////////////////////
+
 	let counter = 0;
 
 	function clickHandler(event) {
-		const newElement = document.createElement('CUSTOM-ELEMENT');
-		newElement.appendChild(document.createTextNode('CUSTOM-ELEMENT-' + counter));
+		const newElement = document.createElement(component);
+		newElement.appendChild(document.createTextNode(`${component}-${counter}`));
 		counter = counter + 1;
 
 		const currentNode = event.target;
@@ -29,25 +25,76 @@
 		}
 	}
 
-	// CREATE ELEMENT THROUGH DOCUMENT.CREATEELEMENT
-	function created(element) {
-		if (!element.ready) {
-			element.ready = true;
-			element.addEventListener('click', clickHandler);
+	/////////////////////////////////////////////////////////////////////////////
+	// ELEMENT PROPERTIES
+	/////////////////////////////////////////////////////////////////////////////
+
+	const elementProperties = {
+		appendedCallback: {
+			value: function() {
+				console.log(`${component} APPENDED`);
+			}
+		},
+
+		attributeChangeCallback: {
+			value: function(attributeName) {
+				console.log(`${component} ATTRIBUTE CHANGED`, attributeName);
+			}
+		},
+
+		createCallback: {
+			value: function() {
+				console.log(`${component} CREATED`);
+
+				this.addEventListener('click', clickHandler);
+			}
+		},
+
+		component: {
+			value: component
+		},
+
+		removedCallback: {
+			value: function() {
+				console.log('REMOVED');
+			}
+		},
+	};
+
+	/////////////////////////////////////////////////////////////////////////////
+	// GLOBAL STYLES
+	/////////////////////////////////////////////////////////////////////////////
+
+	const style = document.head.appendChild(document.createElement('style'));
+	style.appendChild(document.createTextNode('')); // WebKit hack :(
+
+	const sheet = style.sheet;
+	sheet.insertRule(`
+		${component} {
+			color: red;
+			display: block;
 		}
+	`);
+
+	/////////////////////////////////////////////////////////////////////////////
+	// GLOBAL EVENT HANDLER
+	/////////////////////////////////////////////////////////////////////////////
+
+	function setup(element) {
+		if (!element.hasOwnProperty('component')) {
+			Object.defineProperties(element, Object.assign({}, elementProperties, eventProperties));
+			element.createCallback();
+		}
+
+		return element;
 	}
 
-	// APPEND ELEMENT
-	function appended(element) {
-		created(element);
-	}
+	window.on(`elementcreated:${component}`, (element) => setup(element));
+	window.on(`elementadded:${component}`, (element) => setup(element).appendedCallback());
+	window.on(`elementattributechange:${component}`, (element, attributeName) => setup(element).attributeChangeCallback(attributeName));
+	window.on(`elementremoved:${component}`, (element) => setup(element).removedCallback());
 
-	// OBSERVER
-	window.on('elementcreated:CUSTOM-ELEMENT', created);
-	window.on('elementadded:CUSTOM-ELEMENT', appended);
-
-	// INITIALIZE EXISTING ELEMENTS
-	document.querySelectorAll('CUSTOM-ELEMENT').forEach((element) => {
-		appended(element);
+	document.querySelectorAll(component).forEach((element) => {
+		setup(element);
 	});
 })();
